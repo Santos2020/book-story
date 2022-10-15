fdccdf79e0e5fb96519d2b5506d79d61664e9f19
Carlos Santos39 <novasintoniasp@gmail.com>
2022-10-15 14:08:36 -0300

    projeto book criado com banco de dados pgAdmi

---
+++ src/test/java/com/carlos/BookStory/BookStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.BookStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
