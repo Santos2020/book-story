bc5004d30e4a00e5a75156592afb84b37c097f41
Carlos Santos39 <novasintoniasp@gmail.com>
2022-10-15 16:45:27 -0300

    instancia base de dados

--- src/main/java/com/carlos/BookStory/BookStoryApplication.java
+++ src/main/java/com/carlos/BookStory/BookStoryApplication.java
@@ -1,22 +1,42 @@
 package com.carlos.bookstory;
 
+import com.carlos.bookstory.domain.Categoria;
+import com.carlos.bookstory.domain.Livro;
+import com.carlos.bookstory.repositories.CategoriaRepository;
+import com.carlos.bookstory.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @RestController
 @SpringBootApplication
-public class BookStoryApplication {
+public class BookStoryApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoryApplication.class, args);
-	}
-	  @GetMapping("/")
-		public String index(){
-			return "Hello Word";
-
 
 	}
+	@Override
+	public void run( String... args ) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livro");
+		Livro li1 = new Livro(null, "Mais ixperto que Diado", "Napoleao Hill", "Expert",cat1);
+		cat1.getLivro().addAll(Arrays.asList(li1));
 
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(li1));
+
+
+	}
 }
